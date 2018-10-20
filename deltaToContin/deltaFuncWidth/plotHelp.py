e86e7dbf17a824409fb9214ee8b73057a3150011
Amelia Trainer <ameliajo@mit.edu>
2018-10-19 21:17:08 -0400

    added more to description, generated like a lot of plots

--- deltaToContin/deltaFuncWidth/plotHelp.py
+++ deltaToContin/deltaFuncWidth/plotHelp.py
@@ -13,41 +13,31 @@ color2 = [ "#00cc00", "#00c20d", "#00b81a", "#00ad26", "#00a333", "#009940",
 color1shorter = [ "#ff0000", "#ff4c00", "#ff7300", "#ff9900", "#ffbf00", "#ffe600" ]
 color2shorter = [ "#00cc00", "#008f4c", "#007073", "#005299", "#0033bf", "#0014e6" ]
 
-
-
 def plotErrorAgainstBetaForGivenAlpha(a,yDelta,noDelta,alpha,beta):
     f = plt.figure(a)
     for i,n_delta in enumerate(noDelta):
         error = []
-        error2 = []
+        #error2 = []
         index = len(beta)*a
         for b in range(len(beta)):
             y,n = yDelta[index+b], n_delta[index+b]
             #error.append(100.0*abs(y-n)/y)
-            error.append(n)
-            if i == 0: error2.append(y)
-        if i == 0 :plt.plot(beta,error2,color=color2shorter[i+5],label="With Delta Function",linewidth=1)
+            error.append(abs(y-n))
+            #error.append(n)
+            #if i == 0: error2.append(y)
+        #if i == 0 :plt.plot(beta,error2,color=color2shorter[i+5],label="With Delta Function",linewidth=1)
         plt.plot(beta,error,label="Width: "+str(i+1)+" points used",color=color1shorter[i])
 
-    #plt.plot(beta,error2,color=color2shorter[i],label="With Delta Function")
-    #plt.title("Error of Delta vs. Continuous Representation of Single Peak for Alpha = "+str(alpha[a]))
-    #plt.xlabel("Beta Values")
-    #plt.ylabel("Rel. Error (%)")
-
-    #plt.title("Error of Delta vs. Continuous Representation of Single Peak for Alpha = "+str(alpha[a]))
-
-    plt.title("Run #2 NJOY-generated S(a,b) for a = "+str(alpha[a])+" using conventional\ndelta input as well various triangles to approximate delta function")
-    plt.ylabel("Freq. Dist. Values")
+    plt.title(r"Run #2 NJOY-generated Error for $\alpha$ = "+str(alpha[a])+" comparing\ndelta input against various triangles to approximate delta function")
+    #plt.ylabel("Relative Error in %")
+    plt.ylabel("Absolute Error")
     plt.xlabel("Beta Values")
-    x1,x2,y1,y2 = plt.axis()
-    plt.axis((x1,x2,0.0,0.065))
+    #x1,x2,y1,y2 = plt.axis()
+    #plt.axis((x1,x2,0.0,0.07))
 
-    plt.legend(loc='center right')
+    plt.legend(loc='upper right')
 
     f.show()
-
-
-
 
 
 
@@ -56,27 +46,37 @@ def plotErrorAgainstBetaForGivenAlpha(a,yDelta,noDelta,alpha,beta):
 def plotSabAgainstBetaForGivenAlpha(a,yDelta,noDelta,alpha,beta):
 
     f2 = plt.figure(a)
-    for i,n_delta in enumerate(noDelta):
-        error = []
-        error2 = []
+
+    for i,ndelta in enumerate(noDelta):
         index = len(beta)*a
-        for b in range(len(beta)):
-            y,n = yDelta[index+b], n_delta[index+b]
-            error.append(n)
-            if i == 0: error2.append(y)
-        if i == 0 :plt.plot(beta,error2,color=color2shorter[i+5],label="With Delta Function",linewidth=1)
-        plt.plot(beta,error,label="Width: "+str(i+1)+" points used",color=color1shorter[i])
+        deltaSab    = [yDelta[index+b] for b in range(len(beta))]
+        triangleSab = [ndelta[index+b] for b in range(len(beta))]
+
+        if i == 0: plt.plot(beta,deltaSab,color=color2shorter[i+5],label="With Delta Function",linewidth=1)
+        plt.plot(beta,triangleSab,label="Width: "+str(i+1)+" points used",color=color1shorter[i])
 
 
-    plt.title("Run #2 NJOY-generated S(a,b) for a = "+str(alpha[a])+" using conventional\ndelta input as well various triangles to approximate delta function")
-    plt.ylabel("Freq. Dist. Values")
-    plt.xlabel("Beta Values")
-    #x1,x2,y1,y2 = plt.axis()
-    #plt.axis((x1,x2,0.0,0.065))
-
+    plt.title(r"Run #2 NJOY-generated $S(\alpha,\beta)$ for $\alpha$ = "+str(alpha[a])+" using conventional\ndelta input as well various triangles to approximate delta function")
+    plt.ylabel("Freq. Dist. Values"); plt.xlabel("Beta Values")
     plt.legend(loc='center right')
 
     f2.show()
+
+
+
+
+def plotJustWithDeltaFuncs(desiredAlpha,alpha,beta,yDelta):
+    f1 = plt.figure(0)
+    for a in [0,3,6,9,12]:
+        index = int(len(beta))*a
+        deltaSab = [yDelta[index+b] for b in range(len(beta))]
+        plt.plot(beta,deltaSab,color=color2[a],label=r"$\alpha=$"+str(alpha[a]))
+        plt.title("Run #1 NJOY-generated S(a,b) using conventional delta input")
+        plt.ylabel("Freq. Dist. Values"); plt.xlabel("Beta Values")
+
+        plt.legend(loc='center right')
+
+    f1.show()
 
 
 
--- deltaToContin/deltaFuncWidth/run_alphaBeta.py
+++ deltaToContin/deltaFuncWidth/run_alphaBeta.py
@@ -1,42 +1,22 @@
 import matplotlib.pyplot as plt
 from plotHelp import *
-from run1_results import *
+from run2_results import *
 
-color1 = [ "#ff0000", "#ff0d00", "#ff1a00", "#ff2600", "#ff3300", "#ff4000", 
-           "#ff4c00", "#ff5900", "#ff6600", "#ff7300", "#ff8000", "#ff8c00", 
-           "#ff9900", "#ffa600", "#ffb200", "#ffbf00", "#ffcc00", "#ffd900", 
-           "#ffe600", "#fff200", "#ffff00" ]
-color2 = [ "#00cc00", "#00c20d", "#00b81a", "#00ad26", "#00a333", "#009940", 
-           "#008f4c", "#008559", "#007a66", "#007073", "#006680", "#005c8c", 
-           "#005299", "#0047a6", "#003db2", "#0033bf", "#0029cc", "#001fd9", 
-           "#0014e6", "#000af2", "#0000ff" ]
 colors = [color1,color2]
 alpha = [1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0]
 beta = [0.01,0.1,1.0,5.0,10.0,15.0,20.0,25.0,30.0,35.0,40.0,50.0]
+
 
 noDelta = [nDelta_width2,nDelta_width4,nDelta_width6,nDelta_width8,nDelta_width10]
 #plotSabAgainstBetaForGivenAlpha(0,yDelta,noDelta,alpha,beta)
 #plotSabAgainstBetaForGivenAlpha(6,yDelta,noDelta,alpha,beta)
 #plotSabAgainstBetaForGivenAlpha(12,yDelta,noDelta,alpha,beta)
 
-f1 = plt.figure(0)
-for a in [0,3,6,9,12]:
-    error2 = []
-    for b in range(len(beta)):
-        y = yDelta[int(len(beta)*a+b)]
-        error2.append(y)
-    plt.plot(beta,error2,color=color2[a],label=r"$\alpha=$"+str(alpha[a]))
+plotErrorAgainstBetaForGivenAlpha(0,yDelta,noDelta,alpha,beta)
+plotErrorAgainstBetaForGivenAlpha(6,yDelta,noDelta,alpha,beta)
+plotErrorAgainstBetaForGivenAlpha(12,yDelta,noDelta,alpha,beta)
 
-
-    plt.title("Run #1 NJOY-generated S(a,b) using conventional delta input")
-    plt.ylabel("Freq. Dist. Values")
-    plt.xlabel("Beta Values")
-    #x1,x2,y1,y2 = plt.axis()
-    #plt.axis((x1,x2,0.0,0.065))
-
-    plt.legend(loc='center right')
-
-f1.show()
-
+#desiredAlpha = [0,3,7,9,12]
+#plotJustWithDeltaFuncs(desiredAlpha,alpha,beta,yDelta)
 
 input()

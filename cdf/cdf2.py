193cf80ed4fd4010d617f371af18d255d5ae57f0
Amelia Trainer <ameliajo@mit.edu>
2018-10-24 12:25:50 -0400

    cleaning up cdf2, added quick test. making it work for symmetric/nonsymmetric input sab

--- cdf/cdf2.py
+++ cdf/cdf2.py
@@ -1,10 +1,18 @@
 import matplotlib.pyplot as plt
-from sabFreeGas   import *
-from alphaFreeGas import *
-from betaFreeGas  import *
+from sabFreeGasSym   import *
+from sabFreeGas      import *
+from alphaFreeGas    import *
+from betaFreeGas     import *
 import numpy as np
 from math import pi
 from math import exp
+
+sab = sabSym[:]
+
+def getAlphaMinMax(E,beta,kb,T,A):
+    aMin = ( (E)**0.5 - (E+(beta)*kb*T)**0.5 )**2 / ( A*kb*T )
+    aMax = ( (E)**0.5 + (E+(beta)*kb*T)**0.5 )**2 / ( A*kb*T )
+    return aMin,aMax
 
 
 def calcAsym(alpha,beta):
@@ -12,31 +20,19 @@ def calcAsym(alpha,beta):
     return (1.0/(4.0*pi*alpha)**0.5) * exp( - (alpha+beta)**2 / (4.0*alpha) )
 
 
-
-for b in range(len(betas)):
-    beta = betas[b]
-    alphaVec = alphas[b] 
-    for a in range(len(alphaVec)):
-        alpha = alphaVec[a]
-        if abs(sab[b][a]-calcAsym(alpha,beta))>1e-15: 
-            print(beta,alpha,sab[b][a]-calcAsym(alpha,beta))
-
-
-
-def getEq14(beta,E,T,Sab,A,alphas,lenBeta,b):
-    aMin = ( (E)**0.5 - (E+(beta)*kb*T)**0.5 )**2 / ( A*kb*T )
-    aMax = ( (E)**0.5 + (E+(beta)*kb*T)**0.5 )**2 / ( A*kb*T )
-    aIndices = [ i for i in range(len(alphas)) if (alphas[i] >= aMin and alphas[i] <= aMax) ]
-
-    sabVec = [sab[b][a] for a in aIndices]
-    validAlpha = alphaVec[aIndices[0]:aIndices[-1]+1]
-    return np.trapz(sabVec,x=validAlpha)
-
-    #numerator = exp(-beta/2) * np.trapz([sab[b][a] for i in aIndices],x=alphaVec[aIndices[0]:aIndices[-1]+1])
-
-
-
-
+def calcSym(alpha,beta):
+    """
+    >>> abs(calcSym(1.0,2.0)-calcSym(1.0,-2.0)) < 1e-15
+    True
+    >>> abs(calcSym(5.0,12.0)-calcSym(5.0,-12.0)) < 1e-15
+    True
+    >>> abs(calcSym(3.5,5.0)-calcSym(2.5,-5.0)) < 1e-15
+    False
+    >>> abs(calcSym(2.5,4.0)-calcSym(2.5,-5.0)) < 1e-15
+    False
+    """
+    if alpha == 0: alpha = 0.0001
+    return exp(beta*0.5)*calcAsym(alpha,beta)
 
 
 A = 1.0 
@@ -45,27 +41,8 @@ T = 1000    # 300 K
 kb = 8.6173303e-5
 
 bMin, bMax = -E/(kb*T), 20.0
+sabIsSym = True
 
-
-"""
-
-eq14, eq14Neg = [],[]
-eq14Neg = []
-for b,beta in enumerate(betas):
-
-    alphaVec = alphas[b] 
-    eq14.append(getEq14(beta,E,T,sab,A,alphaVec,len(betas),b))
-    eq14Neg.append(getEq14(-beta,E,T,sab,A,alphaVec,len(betas),b))
-    print(eq14Neg[0])
-    break 
-    #eq14.append(getEq14(beta,E,T,sab_water,A,alphas,len(betas),b))
-    #eq14Neg.append(getEq14(-beta,E,T,sab_water,A,alphas,len(betas),b))
-
-#eq14Neg.reverse()
-#eq14Full = eq14Neg + eq14
-#betaFull = 
-
-"""
 
 
 eq14Pos = []
@@ -77,19 +54,21 @@ for b in range(len(betaPosVec)):
     beta = betaPosVec[b]
     bIndex = betas.index(betaPosVec[b])
     alphaVec = alphas[bIndex] 
-    aMin = ( (E)**0.5 - (E+(beta)*kb*T)**0.5 )**2 / ( A*kb*T )
-    aMax = ( (E)**0.5 + (E+(beta)*kb*T)**0.5 )**2 / ( A*kb*T )
+    aMin, aMax = getAlphaMinMax(E,beta,kb,T,A)
     validAlphas = [ a for a in alphaVec if (a >= aMin and a <= aMax) ]
     
     sabVals = []
     for alpha in validAlphas:
         aIndex = alphas[bIndex].index(alpha)
-        if (sab[bIndex][aIndex]-calcAsym(alpha,betaPosVec[b])) > 1e-15: print("OH NO")
+        if     sabIsSym and (sab[bIndex][aIndex]-calcSym(alpha,beta))  > 1e-15: print("OH NO")
+        if not sabIsSym and (sab[bIndex][aIndex]-calcAsym(alpha,beta)) > 1e-15: print("OH NO")
         sabVals.append(sab[bIndex][aIndex])
-    numerator = np.trapz(sabVals,x=validAlphas)
-    eq14Pos.append(numerator)
-
-#print(eq14Pos)
+    if sabIsSym:
+        numerator = exp(-0.5*beta)*np.trapz(sabVals,x=validAlphas)
+        eq14Pos.append(numerator)
+    if not sabIsSym:
+        numerator = np.trapz(sabVals,x=validAlphas)
+        eq14Pos.append(numerator)
 
  
 eq14Neg = []
@@ -101,25 +80,29 @@ for b in range(len(betaNegVec)):
     beta = betaNegVec[b]
     bIndex = betas.index(-betaNegVec[b])
     alphaVec = alphas[bIndex] 
-    aMin = ( (E)**0.5 - (E+(-beta)*kb*T)**0.5 )**2 / ( A*kb*T )
-    aMax = ( (E)**0.5 + (E+(-beta)*kb*T)**0.5 )**2 / ( A*kb*T )
+    aMin, aMax = getAlphaMinMax(E,-beta,kb,T,A)
     validAlphas = [ a for a in alphaVec if (a >= aMin and a <= aMax) ]
     
     sabVals = []
     for alpha in validAlphas:
         aIndex = alphas[bIndex].index(alpha)
-        if (sab[bIndex][aIndex]-calcAsym(alpha,-betaNegVec[b])) > 1e-15: print("OH NO")
+        if     sabIsSym and (sab[bIndex][aIndex]-calcSym(alpha,-beta))  > 1e-15: print("OH NO")
+        if not sabIsSym and (sab[bIndex][aIndex]-calcAsym(alpha,-beta)) > 1e-15: print("OH NO")
         sabVals.append(sab[bIndex][aIndex])
-    numerator = np.trapz(sabVals,x=validAlphas)
-    eq14Neg.append(numerator)
+    if sabIsSym:
+        numerator = exp(0.5*beta)*np.trapz(sabVals,x=validAlphas)
+        eq14Neg.append(numerator)
+    if not sabIsSym:
+        numerator = np.trapz(sabVals,x=validAlphas)
+        eq14Neg.append(numerator)
+
 
        
 eq14Neg.reverse()
-betaNegVec.reverse()
 print(eq14Neg)
+betaNegVec.reverse()
 eq14 = eq14Neg + eq14Pos
 betaTotal = [-b for b in betaNegVec] + betaPosVec
-print(betaTotal)
 invIntegral = 1.0/np.trapz(eq14, x=betaTotal)
 eq14 = [x * invIntegral for x in eq14]
 f = plt.figure(1)
@@ -128,4 +111,7 @@ f.show()
 hold = input()
 
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
         
--- cdf/freeGas.py
+++ cdf/freeGas.py
@@ -9,6 +9,10 @@ def calcAsym(alpha,beta):
     if alpha == 0: alpha = 0.0001
     return (1.0/(4.0*pi*alpha)**0.5) * exp( - (alpha+beta)**2 / (4.0*alpha) )
 
+def calcSym(alpha,beta):
+    if alpha == 0: alpha = 0.0001
+    return exp(beta*0.5)*calcAsym(alpha,beta)
+
 
 A = 1.0     # H2O = 1.01 + 1.01 + 16.00 = 18.02 
 E = 1.0     # 1 eVc
@@ -32,6 +36,10 @@ aFile.write("alphas = [")
 sabFile = open("sabFreeGas.py", "w")
 sabFile.write("sab = [")
 
+sabSymFile = open("sabFreeGasSym.py", "w")
+sabSymFile.write("sabSym = [")
+
+
 
 numAlpha = 100.0
 eq14 = []
@@ -48,6 +56,7 @@ for b in beta:
         #aFile.write(str([float("%.5f"%round(a,6)) for a in alpha])+", ")
         aFile.write(str([a for a in alpha])+", ")
     sabVec = [calcAsym(a,b) for a in alpha]
+    sabSymVec = [calcSym(a,b) for a in alpha]
 
     if b == beta[-1]:
         #sabFile.write(str([float("%.5f"%round(sab,6)) for sab in sabVec])+"]")
@@ -56,15 +65,19 @@ for b in beta:
         #sabFile.write(str([float("%.5f"%round(sab,6)) for sab in sabVec])+", ")
         sabFile.write(str([sab for sab in sabVec])+", ")
 
+    if b == beta[-1]: sabSymFile.write(str([sab for sab in sabSymVec])+"]")
+    else: sabSymFile.write(str([sab for sab in sabSymVec])+", ")
+
+
     #eq14.append(sum([calcAsym(a,b)*(alpha_max-alpha_min)/numAlpha for a in alpha]))
     eq14.append(np.trapz(sabVec,x=alpha))
+    #eq14.append(exp(-b/2)*(np.trapz(sabSymVec,x=alpha)))
     if ( b <= 0 ) : 
         print(eq14[-1])
     
     
 aFile.close()
 
-"""
 invIntegral = 1.0/np.trapz(eq14, x=beta)
 eq14 = [ x * invIntegral for x in eq14 ]
 
@@ -72,6 +85,7 @@ f = plt.figure(1)
 plt.plot(beta,eq14)
 f.show()
 hold = input()
+"""
 
 
 """

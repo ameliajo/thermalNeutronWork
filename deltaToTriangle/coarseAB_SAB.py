f07bde44960b18dc349535b0c93c6ccb12250cdc
Amelia Trainer <ameliajo@mit.edu>
2018-12-26 20:10:20 -0500

    made coarseAB.py more flexible (it can call NJOY now too). Also starting writeup to describe what all I'm doing

--- deltaToTriangle/coarseAB_SAB.py
+++ deltaToTriangle/coarseAB_SAB.py
@@ -4,24 +4,46 @@ import sys
 import subprocess
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
+from generateNjoyInput import *
+
 
 ##############################################################################
 # READ IN DATA FROM NJOY-GENERATED FILE
 ##############################################################################
+alphas = [0.001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7 ]
+betas = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.6, 7.7, 7.8, 7.9, 8, 8.05, 8.1, 8.15, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 9, 10, 12, 14, 16, 18, 20]
+continRho = [0, .0005, .001, .002, .0035, .005, .0075, .01, .013, .0165, .02,  \
+  .0245, .029, .034, .0395, .045, .0506, .0562, .0622, .0686, .075, .083, .091,\
+  .099, .107, .115, .1197, .1214, .1218, .1195, .1125, .1065, .1005, .09542,   \
+  .09126, .0871, .0839, .0807, .07798, .07574, .0735, .07162, .06974, .06804,  \
+  .06652, .065, .0634, .0618, .06022, .05866, .0571, .05586, .05462, .0535,    \
+  .0525, .0515, .05042, .04934, .04822, .04706, .0459, .04478, .04366, .04288, \
+  .04244, .042, 0.0]
+
+
+
 if len(sys.argv) > 1:
     if sys.argv[1] == 'njoy':
-        subprocess.run(['cp','/Users/amelia/NJOY2016/bin/sabContin.txt','./'])
-        subprocess.run(['cp','/Users/amelia/NJOY2016/bin/sabDelta.txt','./'])
+        deltaName  = 'Delta'
+        continName = 'Contin'
+        # Generate S(a,b) using delta functions
+        generateNjoyInput(deltaName,alphas,betas,continRho,True)
+        runNJOY(deltaName)
+        # Generate S(a,b) by approximating delta functions as a very thin triangle
+        generateNjoyInput(continName,alphas,betas,getPhononDist(2,continRho),False)
+        runNJOY(continName)
+
+
 
 def getLine(f):
     return [float(num) for num in f.readline().split()]
 
-with open('sabContin.txt','r') as f:
+with open('sabResults/sab_Contin.txt','r') as f:
     sabCONTIN = getLine(f)
     alphaVals = getLine(f)
     betaVals  = getLine(f)
 
-with open('sabDelta.txt','r') as f:
+with open('sabResults/sab_Delta.txt','r') as f:
     sabDELTA   = getLine(f)
     assert(getLine(f) == alphaVals)
     assert(getLine(f) == betaVals)
--- deltaToTriangle/makeTest09Rho.py
+++ deltaToTriangle/makeTest09Rho.py
@@ -60,13 +60,23 @@ def getPhononDist(width,continRho):
 
 
 if __name__=="__main__":
+    """
+    plt.plot(E,getPhononDist(0,continRho),'r')
+    plt.plot([0.205,0.205],[0.0,70.0],'r')
+    plt.plot([0.48,0.48],[0.0,140.0],'r')
+    plt.xlabel('Energy (eV)')
+    plt.ylabel('Freq. Distribution')
+    plt.title('Phonon Distribution for H in H2O (delta functions of arbitrary height)\n as prepared for NJOY 2016 Test09')
+    plt.show()
+    """
+
     for i in range(2,12,2):
-        plt.plot(E,getPhononDist(i,continRho))
+        #plt.plot(E,getPhononDist(i,continRho))
         print(i)
         #print(['%.5E'%x for x in getPhononDist(i,continRho)] )
         print(np.array(getPhononDist(i,continRho)))
         print()
-    plt.show()
+    #plt.show()
 
 
 

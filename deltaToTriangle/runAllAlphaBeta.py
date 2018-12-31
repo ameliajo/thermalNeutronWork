00d1a3276e5fd5de3a0f7dbb3c63e1c526b99c3b
Amelia Trainer <ameliajo@mit.edu>
2018-12-31 10:15:09 -0500

    cleaned up runAllALphaBeta

--- deltaToTriangle/runAllAlphaBeta.py
+++ deltaToTriangle/runAllAlphaBeta.py
@@ -6,6 +6,18 @@ import matplotlib.colors as colors
 import matplotlib.cm as cmx
 
 
+
+def prepPlot():
+    cnorm = colors.Normalize(vmin=0,vmax=len(alphas)+3)
+    scalarMap = cmx.ScalarMappable(norm=cnorm,cmap=plt.get_cmap('hot')) #hot autumn tab10
+    mymap = colors.LinearSegmentedColormap.from_list('funTestColors',\
+            [scalarMap.to_rgba(a) for a in range(len(alphas))])
+    colorBar = plt.contourf([[0,0],[0,0]], alphas, cmap=mymap)
+    plt.clf()
+    return scalarMap, colorBar
+
+
+
 def finishPlotting(colorBar,title):
     ax = plt.gca()
     plt.colorbar(colorBar).ax.set_ylabel('alpha values')
@@ -16,13 +28,62 @@ def finishPlotting(colorBar,title):
 
 
 
-def runAlphasBetas(option):
+def runAlphasBetas(option,alphas,betas,continRho,oscE,oscW,fullRedo=False):
     # Option
     # (1)  My  LEAPR delta function vs NJOY LEAPR delta function
     # (2)  My  LEAPR thin triangle  vs NJOY LEAPR thin triangle
     # (3)  My  LEAPR delta function vs  MY  LEAPR thin triangle
     # (4) NJOY LEAPR delta function vs NJOY LEAPR thin triangle
 
+    A0 = 18.02
+    E = 0.01 
+    kbT = 0.025851
+
+    titleIntro = 'S(a,b) for H in H2O, generated with '
+    titleErrorIntro = 'S(a,b) error for H in H2O, generated with '
+
+    if option == 1: # MY LEAPR DELTA FUNCS VS NJOY LEAPR DELTA FUNCS
+        NJOY_LEAPR_1 = False; width_1 = None
+        NJOY_LEAPR_2 = True;  width_2 = None
+        title = 'my\nLEAPR vs. NJOY LEAPR. Using delta function approx.'
+
+    if option == 2: # MY LEAPR CONTIN2 FUNCS VS NJOY LEAPR CONTIN2 FUNCS
+        NJOY_LEAPR_1 = False; width_1 = 2
+        NJOY_LEAPR_2 = True;  width_2 = 2 
+        title = 'my\nLEAPR vs. NJOY LEAPR. Using thin triangle approx.'
+
+    if option == 3: # MY LEAPR DELTA FUNCS VS MY LEAPR CONTIN2 FUNCS
+        NJOY_LEAPR_1 = False; width_1 = None 
+        NJOY_LEAPR_2 = False; width_2 = 2 
+        title = 'my LEAPR.\nUsing delta func. approx vs. thin triangle approx.'
+ 
+    if option == 4: # NJOY LEAPR DELTA FUNCS VS NJOY LEAPR CONTIN2 FUNCS
+        NJOY_LEAPR_1 = True; width_1 = None 
+        NJOY_LEAPR_2 = True; width_2 = 2 
+        title = 'NJOY LEAPR.\nUsing delta func. approx vs. thin triangle approx.'
+
+    scalarMap, colorBar = prepPlot()
+    ########################################################################
+    # Generate specified S(a,b)
+    SAB_1 = getSAB(alphas,betas,continRho,NJOY_LEAPR_1,fullRedo,width_1,oscE,oscW)
+    SAB_2 = getSAB(alphas,betas,continRho,NJOY_LEAPR_2,fullRedo,width_2,oscE,oscW)
+    ########################################################################
+    # Compare S(a,b) values
+    plotBetaForVariousAlpha(alphas,betas,SAB_1,A0,E,kbT,scalarMap,'.',False)
+    plotBetaForVariousAlpha(alphas,betas,SAB_2,A0,E,kbT,scalarMap,'.',True)
+    finishPlotting(colorBar,titleIntro+title)
+    ########################################################################
+    # Compare S(a,b) errors
+    plotErrorBetaForVariousAlpha(alphas,betas,SAB_1,SAB_2,A0,E,kbT,scalarMap)
+    finishPlotting(colorBar,titleErrorIntro+title)
+    ########################################################################
+
+
+
+
+
+
+if __name__=="__main__":
     alphas = [0.001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7 ]
     betas = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.6, 7.7, 7.8, 7.9, 8, 8.05, 8.1, 8.15, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 9, 10, 12, 14, 16, 18, 20]
     continRho = [0, .0005, .001, .002, .0035, .005, .0075, .01, .013, .0165, .02,  \
@@ -36,115 +97,6 @@ def runAlphasBetas(option):
     oscE = [ 0.204,    0.4794   ] 
     oscW = [ 0.166667, 0.333333 ]
 
-
-    A0 = 18.02
-    E = 0.01 
-    kbT = 0.025851
-
-    cnorm = colors.Normalize(vmin=0,vmax=len(alphas)+3)
-    scalarMap = cmx.ScalarMappable(norm=cnorm,cmap=plt.get_cmap('hot')) #hot autumn tab10
-    mymap = colors.LinearSegmentedColormap.from_list('funTestColors',\
-            [scalarMap.to_rgba(a) for a in range(len(alphas))])
-    colorBar = plt.contourf([[0,0],[0,0]], alphas, cmap=mymap)
-    plt.clf()
-
-    fullRedo = False
-
-    # (1)  My  LEAPR delta function vs NJOY LEAPR delta function
-    # (2)  My  LEAPR thin triangle  vs NJOY LEAPR thin triangle
-    # (3)  My  LEAPR delta function vs  MY  LEAPR thin triangle
-    # (4) NJOY LEAPR delta function vs NJOY LEAPR thin triangle
-
-    if option == 1:
-        ########################################################################
-        # COMPARE MY LEAPR DELTA FUNCS AGAINST NJOY LEAPR DELTA FUNCS
-        ########################################################################
-        MY_DELTA_SAB   = getSAB(alphas,betas,continRho,NJOY_LEAPR=False,\
-                                fullRedo=fullRedo,width=None,oscE=oscE,oscW=oscW)
-        NJOY_DELTA_SAB = getSAB(alphas,betas,continRho,NJOY_LEAPR=True,\
-                                fullRedo=fullRedo,width=None,oscE=oscE,oscW=oscW)
-        ########################################################################
-        # Compare S(a,b) values
-        plotBetaForVariousAlpha(alphas,betas,MY_DELTA_SAB,A0,E,kbT,scalarMap,'.',False)
-        plotBetaForVariousAlpha(alphas,betas,NJOY_DELTA_SAB,A0,E,kbT,scalarMap,'.',True)
-        finishPlotting(colorBar,'S(a,b) for H in H2O, generated using my LEAPR vs. NJOY LEAPR\nUsing delta function approx.')
-        ########################################################################
-        # Compare S(a,b) errors
-        plotErrorBetaForVariousAlpha(alphas,betas,MY_DELTA_SAB,NJOY_DELTA_SAB,A0,E,kbT,scalarMap)
-        finishPlotting(colorBar,'S(a,b) error for H in H2O, generated using my LEAPR vs. NJOY LEAPR\nUsing delta function approx.')
-        ########################################################################
-
-
-    if option == 2:
-        ########################################################################
-        # COMPARE MY LEAPR CONTIN2 FUNCS AGAINST NJOY LEAPR CONTIN2 FUNCS
-        ########################################################################
-        MY_CONTIN2_SAB   = getSAB(alphas,betas,continRho,NJOY_LEAPR=False,\
-                                fullRedo=fullRedo,width=2,oscE=oscE,oscW=oscW)
-        NJOY_CONTIN2_SAB = getSAB(alphas,betas,continRho,NJOY_LEAPR=True,\
-                                fullRedo=fullRedo,width=2,oscE=oscE,oscW=oscW)
-        ########################################################################
-        # Compar S(a,b) values
-        plotBetaForVariousAlpha(alphas,betas,MY_CONTIN2_SAB,A0,E,kbT,scalarMap,'.',False)
-        plotBetaForVariousAlpha(alphas,betas,NJOY_CONTIN2_SAB,A0,E,kbT,scalarMap,'.',True)
-        finishPlotting(colorBar,'S(a,b) for H in H2O, generated using my LEAPR vs. NJOY LEAPR\nUsing thin triangle approx.')
-        ########################################################################
-        # Compar S(a,b) errors
-        ########################################################################
-        plotErrorBetaForVariousAlpha(alphas,betas,MY_CONTIN2_SAB,NJOY_CONTIN2_SAB,A0,E,kbT,scalarMap)
-        finishPlotting(colorBar,'S(a,b) error for H in H2O, generated using my LEAPR vs. NJOY LEAPR\nUsing thin triangle approx.')
-        ########################################################################
-
-
-    if option == 3:
-        ########################################################################
-        # COMPARE MY LEAPR DELTA FUNCS AGAINST MY LEAPR CONTIN2 FUNCS
-        ########################################################################
-        MY_DELTA_SAB   = getSAB(alphas,betas,continRho,NJOY_LEAPR=False,\
-                                fullRedo=fullRedo,width=None,oscE=oscE,oscW=oscW)
-        MY_CONTIN2_SAB = getSAB(alphas,betas,continRho,NJOY_LEAPR=False,\
-                                fullRedo=fullRedo,width=2,oscE=oscE,oscW=oscW)
-        ########################################################################
-        # Compar S(a,b) values
-        plotBetaForVariousAlpha(alphas,betas,MY_DELTA_SAB,A0,E,kbT,scalarMap,'.',False)
-        plotBetaForVariousAlpha(alphas,betas,MY_CONTIN2_SAB,A0,E,kbT,scalarMap,'.',True)
-        finishPlotting(colorBar,'S(a,b) for H in H2O, generated using my LEAPR v\nUsing thin triangle approx. vs delta func. approx.')
-        ########################################################################
-        # Compar S(a,b) errors
-        ########################################################################
-        plotErrorBetaForVariousAlpha(alphas,betas,MY_DELTA_SAB,MY_CONTIN2_SAB,A0,E,kbT,scalarMap)
-        finishPlotting(colorBar,'S(a,b) error for H in H2O, generated using my LEAPR v\nUsing thin triangle approx. vs delta func. approx.')
-        ########################################################################
-    
-
-    if option == 4:
-        ########################################################################
-        # COMPARE NJOY LEAPR DELTA FUNCS AGAINST NJOY LEAPR CONTIN2 FUNCS
-        ########################################################################
-        NJOY_DELTA_SAB   = getSAB(alphas,betas,continRho,NJOY_LEAPR=True,\
-                                fullRedo=fullRedo,width=None,oscE=oscE,oscW=oscW)
-        NJOY_CONTIN2_SAB = getSAB(alphas,betas,continRho,NJOY_LEAPR=True,\
-                                fullRedo=fullRedo,width=2,oscE=oscE,oscW=oscW)
-        ########################################################################
-        # Compar S(a,b) values
-        plotBetaForVariousAlpha(alphas,betas,NJOY_DELTA_SAB,A0,E,kbT,scalarMap,'.',False)
-        plotBetaForVariousAlpha(alphas,betas,NJOY_CONTIN2_SAB,A0,E,kbT,scalarMap,'.',True)
-        finishPlotting(colorBar,'S(a,b) for H in H2O, generated using NJOY LEAPR v\nUsing thin triangle approx. vs delta func. approx.')
-        ########################################################################
-        # Compar S(a,b) errors
-        ########################################################################
-        plotErrorBetaForVariousAlpha(alphas,betas,NJOY_DELTA_SAB,NJOY_CONTIN2_SAB,A0,E,kbT,scalarMap)
-        finishPlotting(colorBar,'S(a,b) error for H in H2O, generated using NJOY LEAPR v\nUsing thin triangle approx. vs delta func. approx.')
-        ###########################################################################
-
-
-
-
-
-
-
-
-if __name__=="__main__":
     question = \
     " (1)  My  LEAPR delta function vs NJOY LEAPR delta function \n" + \
     " (2)  My  LEAPR thin triangle  vs NJOY LEAPR thin triangle \n"  + \
@@ -154,5 +106,5 @@ if __name__=="__main__":
 
 
     option = int(input(question))
-    runAlphasBetas(option)
+    runAlphasBetas(option,alphas,betas,continRho,oscE,oscW)
 

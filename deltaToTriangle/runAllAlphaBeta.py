a09742965bcde4a0ac176ba689bb19725d46ebae
Amelia Trainer <ameliajo@mit.edu>
2019-01-09 11:03:19 -0500

    something spooky is happening. second delta function doesn't seem to affect s(a,b). check that out

--- deltaToTriangle/runAllAlphaBeta.py
+++ deltaToTriangle/runAllAlphaBeta.py
@@ -67,8 +67,8 @@ def runAlphasBetas(opt0,opt1,alphas,betas,continRho,oscE,oscW,fullRedo=False):
 
 
 if __name__=="__main__":
-    alphas = [0.001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7 ]
-    betas = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.6, 7.7, 7.8, 7.9, 8, 8.05, 8.1, 8.15, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 9, 10, 12, 14, 16, 18, 20]
+    alphas = [0.001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 2.0, 3.0, 4.0 ]
+    betas = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.6, 7.7, 7.8, 7.9, 8, 8.05, 8.1, 8.15, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 9, 10, 12, 14, 16,17, 18, 18.8, 18.9,19,19.1, 19.2, 20]
     continRho = [0, .0005, .001, .002, .0035, .005, .0075, .01, .013, .0165, .02,  \
       .0245, .029, .034, .0395, .045, .0506, .0562, .0622, .0686, .075, .083, .091,\
       .099, .107, .115, .1197, .1214, .1218, .1195, .1125, .1065, .1005, .09542,   \
@@ -80,15 +80,16 @@ if __name__=="__main__":
     oscE = [ 0.204,    0.4794   ] 
     oscW = [ 0.166667, 0.333333 ]
 
+    fullRedo = True
     opt0,opt1 = ('mine',None),('mine',2) # Works
-    opt0,opt1 = ('njoy',None),('njoy',2) # Works
-    opt0,opt1 = ('mine',None),('njoy',None) # Works
+    #opt0,opt1 = ('njoy',None),('njoy',2) # Works
+    #opt0,opt1 = ('mine',None),('njoy',None) # Works
     #opt0,opt1 = ('mine',2),('njoy',2) # Works
     #opt0,opt1 = ('mine',None),('njoy',2) # FAILS, diagonal comparison
     #opt0,opt1 = ('njoy',None),('mine',2) # FAILS, diagonal comparison
     #opt0,opt1 = ('mine',None),('mine',None) # FAILS, identical
     #opt0,opt1 = ('njoy',2),('njoy',2) # FAILS, identical
     #opt0,opt1 = ('test',None),('njoy',2) # FAILS, bad LEAPR identifier
-    runAlphasBetas(opt0,opt1,alphas,betas,continRho,oscE,oscW)
+    runAlphasBetas(opt0,opt1,alphas,betas,continRho,oscE,oscW,fullRedo)
 
 

98909af4aa95777c8a7de2748a728ffefb5547c5
Amelia Trainer <ameliajo@mit.edu>
2019-03-01 10:18:55 -0500

    continued calcxs stuff. kind of starting over a bit so this is less reliant on old code

--- cdf/sampleXS/calcXS.py
+++ cdf/sampleXS/calcXS.py
@@ -1,102 +1,115 @@
-#from CDF_alpha import *
-from CDF_beta import *
-from random import random
+import numpy as np
 import matplotlib.pyplot as plt
-from rho import continRho
+from math import pi
 from getSAB import *
-from alpha_CDF_no_bound_checking import *
+from rho import continRho,oscE,oscW
 
-oscE = [ 0.204,    0.4794   ] 
-oscW = [ 0.166667, 0.333333 ]
-
-
-
-#for i in range(len(alphas_1)): assert(abs(alphas_1[i]-alphas_2[i]) < 1e-6)
-#for i in range(len(betas_1)):  assert(abs(betas_1[i] -betas_2[i])  < 1e-6)
-#for i in range(len(temps_1)):  assert(abs(temps_1[i] -temps_2[i])  < 1e-6)
+def getSABval(sab,a,b,n_beta):
+    return sab[a*n_beta+b]
 
 
-def getAlphaMinMax(E,beta,T,A):
-    kb = 8.6173303e-5
-    return ( (E)**0.5 - (E+beta*kb*T)**0.5 )**2 / ( A*kb*T ), \
-           ( (E)**0.5 + (E+beta*kb*T)**0.5 )**2 / ( A*kb*T )
-
-def getAlphaMinMaxIndices(E,beta,T,A,alphas):
-    aMin,aMax = getAlphaMinMax(E,beta,T,A)
-    aMin_index, aMax_index = 0, 0
+def getFullSAB(alphas,betas,fullBetas,sabs): 
+    fullSAB = [0.0]*(2*len(betas)-1)*len(alphas)
     for a in range(len(alphas)):
-        if alphas[a] > aMin and aMin_index == 0: aMin_index = a
-        if alphas[a] <= aMax:                    aMax_index = a
-    return aMin_index,aMax_index
+        for b in range(len(fullBetas)):
+            bForLookup = abs(b-len(betas)+1)
+            assert(abs(fullBetas[b]) == betas[bForLookup])
+            # This creates the symmetric SAB
+            fullSAB[a*len(fullBetas)+b] = getSABval(sabs,a,bForLookup,len(betas))
+            # We make it asymmetric to make sure it matches eq14
+            if (fullBetas[b] > 0): fullSAB[a*len(fullBetas)+b] *= np.exp(-fullBetas[b])
+    return fullSAB
 
 
-alphas = alphas_2
-betas = betas_2
-temps = temps_2
-
-t = 0
-
-kb = 8.6173303e-5
-T = temps[0]
-#CDF_alpha = alphaCDF[t]
-CDF_beta = betaCDF[t]
-E = 1.0
-A = 0.98
-
-betaMin = -E/(kb*T)
-betaMax = 20.0
-
-N = 100
-
-fullBetas = [-x for x in betas[::-1]] + betas[1:]
-betas = [x for x in fullBetas if betaMin <=x <betaMax]
-
-
-selectedAlphas = []
-selectedBetas = []
-for i in range(N):
-    xsi_1 = random()
-    index = 0
-    beta = 0.0
-    for i in range(len(CDF_beta)-1):
-        if CDF_beta[i] <= xsi_1 < CDF_beta[i+1]:
-            index = i
-            fraction = (xsi_1 - CDF_beta[i]) / (CDF_beta[i+1] - CDF_beta[i])
-            print(len(betas),len(CDF_beta))
-            beta = (betas[i+1] - betas[i]) * fraction + betas[i]
-            break
-    
-
-    sabsMINE = [getSAB(alphas,betas,T,continRho,NJOY_LEAPR=True,fullRedo=False,\
-                width=None,oscE=oscE,oscW=oscW) for T in temps]
-    CDF_alpha = PDF_CDF_at_various_temps(A,E,temps,beta,alphas,len(betas),index,sabsMINE)[t]
-
-
-    aMin_index,aMax_index = getAlphaMinMaxIndices(E,beta,T,A,alphas)
-    alphasToPlot = []
-    H = []
-    for a in range(aMin_index,aMax_index+1):
-        alphasToPlot.append(alphas[a])
-        H.append( (CDF_alpha[a]-CDF_alpha[aMin_index]) / \
-                  (CDF_alpha[aMax_index]-CDF_alpha[aMin_index]) )
+def getAlphaMinMax(E,beta,kb,T,A):
+    aMin = ( (E)**0.5 - (E+beta*kb*T)**0.5 )**2 / ( A*kb*T )
+    aMax = ( (E)**0.5 + (E+beta*kb*T)**0.5 )**2 / ( A*kb*T )
+    return aMin,aMax
 
 
 
-    xsi_2 = random()
-    alpha = 0.0
-    for i in range(len(CDF_alpha)-1):
-        if CDF_alpha[i] <= xsi_2 < CDF_alpha[i+1]:
-            fraction = (xsi_2 - CDF_alpha[i]) / (CDF_alpha[i+1] - CDF_alpha[i])
-            alpha = (alphas[i+1] - alphas[i]) * fraction + alphas[i]
-            break
+
+def getValidBetasRange(A,E,T,fullBetas):
+    kb = 8.6173303e-5
+    betaMin, betaMax = -E/(kb*T), 20.0
+    bMin = bMax  = len(fullBetas)
+    for b in range(len(fullBetas)):
+        if bMin == len(fullBetas) and betaMin <= fullBetas[b]:
+            bMin = b
+        if fullBetas[b] >= betaMax:
+            return bMin,b
+    return bMin,bMax
 
 
-    #print(alpha,beta)
-    selectedAlphas.append(alpha)
-    selectedBetas.append(beta)
+def intSABda(A,E,T,fullSAB,fullBetas,b):
+    kb = 8.6173303e-5
+    aMin,aMax = getAlphaMinMax(E,fullBetas[b],kb,T,A)
+    denominator = 0.0
+    for a in range(len(alphas)-1):
+        if aMin <= alphas[a] <= aMax:
+            sabL = getSABval(fullSAB,a,b,len(fullBetas))
+            sabR = getSABval(fullSAB,a+1,b,len(fullBetas))
+            denominator += (sabL+sabR)*0.5*(alphas[a+1]-alphas[a])
+    return denominator
 
-plt.plot(selectedAlphas,selectedBetas,'ro')
-plt.show()
+def calcBetaPDF(A,E,T,fullSAB,fullBetas,bMin,bMax):
+    delta0 = fullBetas[bMin+1]-fullBetas[bMin]
+    deltaN = fullBetas[bMax-1]-fullBetas[bMax-2]
+    denominator = intSABda(A,E,T,fullSAB,fullBetas,bMin)*delta0*0.5 +\
+                  intSABda(A,E,T,fullSAB,fullBetas,bMax-1)*deltaN*0.5
+    for b in range(bMin+1,bMax-1):
+        deltaL = fullBetas[b]-fullBetas[b-1]
+        deltaR = fullBetas[b+1]-fullBetas[b]
+        denominator += intSABda(A,E,T,fullSAB,fullBetas,b) * (deltaL+deltaR) * 0.5
+
+    eq14 = [ intSABda(A,E,T,fullSAB,fullBetas,b) / denominator \
+           for b in range(bMin,bMax-1)]
+    return eq14
+
+
+
+def calcBetaCDF(betas,eq14,bMin):
+    eq16 = [0.0]*len(eq14)
+    for b in range(1,len(eq14)):
+        eq16[b] = eq16[b-1] + (eq14[b-1]+eq14[b])*0.5 * \
+                  (fullBetas[bMin+b]-fullBetas[bMin+b-1]) 
+    return eq16
+
+
+
+
+
+def PDF_CDF_at_various_temperatures(A,E,T,fullSAB,fullBetas):
+    bMin,bMax = getValidBetasRange(A,E,T,fullBetas)
+    eq14 = calcBetaPDF(A,E,T,fullSAB,fullBetas,bMin,bMax)
+    eq16 = calcBetaCDF(fullBetas,eq14,bMin)
+    return fullBetas[bMin:bMax-1],eq16
+
+
+
+
+E, A, T = 1.0, 1.0, 296.0
+
+n_alpha, n_beta = 500, 100
+alphas = list(np.linspace(0.01,60,n_alpha))
+betas  = list(np.linspace(0.00,40,n_beta))
+
+useNJOY  = True
+fullRedo = False
+width    = None 
+
+
+fullBetas = [-x for x in betas[1:]][::-1] + betas
+SAB = getSAB(alphas,betas,T,continRho,useNJOY,fullRedo,width,oscE,oscW) 
+fullSAB = getFullSAB(alphas,betas,fullBetas,SAB)
+
+
+
+relevantBetas, betaCDF = PDF_CDF_at_various_temperatures(A,E,T,fullSAB,fullBetas)
+print(len(relevantBetas),len(betaCDF))
+#plt.plot(relevantBetas,betaCDF)
+#plt.show()
+
 
 
 

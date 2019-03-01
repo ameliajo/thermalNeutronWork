cddcdda76ed21a2c70e262a73d8a3fd33e4da807
Amelia Trainer <ameliajo@mit.edu>
2019-03-01 11:08:17 -0500

    getting function (maybe not correct) sampling of a,b

--- cdf/sampleXS/calcXS.py
+++ cdf/sampleXS/calcXS.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from math import pi
 from getSAB import *
 from rho import continRho,oscE,oscW
+from random import random
 
 def getSABval(sab,a,b,n_beta):
     return sab[a*n_beta+b]
@@ -25,6 +26,16 @@ def getAlphaMinMax(E,beta,kb,T,A):
     aMin = ( (E)**0.5 - (E+beta*kb*T)**0.5 )**2 / ( A*kb*T )
     aMax = ( (E)**0.5 + (E+beta*kb*T)**0.5 )**2 / ( A*kb*T )
     return aMin,aMax
+
+
+def getAlphaMinMaxIndices(E,beta,kb,T,A,alphas):
+    aMin,aMax = getAlphaMinMax(E,beta,kb,T,A)
+    aMin_index, aMax_index = 0, 0
+    for a in range(len(alphas)):
+        if alphas[a] > aMin and aMin_index == 0: aMin_index = a
+        if alphas[a] <= aMax:                    aMax_index = a
+    return aMin_index,aMax_index
+
 
 
 
@@ -89,6 +100,7 @@ def PDF_CDF_at_various_temperatures(A,E,T,fullSAB,fullBetas):
 
 
 E, A, T = 1.0, 1.0, 296.0
+kb = 8.6173303e-5
 
 n_alpha, n_beta = 500, 100
 alphas = list(np.linspace(0.01,60,n_alpha))
@@ -103,12 +115,69 @@ fullBetas = [-x for x in betas[1:]][::-1] + betas
 SAB = getSAB(alphas,betas,T,continRho,useNJOY,fullRedo,width,oscE,oscW) 
 fullSAB = getFullSAB(alphas,betas,fullBetas,SAB)
 
-
-
 relevantBetas, betaCDF = PDF_CDF_at_various_temperatures(A,E,T,fullSAB,fullBetas)
-print(len(relevantBetas),len(betaCDF))
+
+N = 1000
+for n in range(N):
+    xsi_1 = random()
+    index = 0
+    beta = 0.0
+    for i in range(len(relevantBetas)-1):
+        if betaCDF[i] <= xsi_1 < betaCDF[i+1]:
+            index = i
+            frac  = (xsi_1-betaCDF[i])/(betaCDF[i+1]-betaCDF[i])
+            beta = frac*(betaCDF[i+1]-betaCDF[i])+relevantBetas[i]
+            break
+
+
+    denominator = 0.0
+    for a in range(len(alphas)-1):
+        sabL = getSABval(fullSAB,a,index,len(fullBetas))
+        sabR = getSABval(fullSAB,a+1,index,len(fullBetas))
+        denominator += (sabL+sabR)*0.5*(alphas[a+1]-alphas[a])
+    alphaPDF = [getSABval(fullSAB,a,index,len(fullBetas))/denominator \
+                for a in range(len(alphas))]
+    
+    alphaCDF = [0.0]*len(alphaPDF)
+    for a in range(1,len(alphaPDF)):
+        alphaCDF[a] = alphaCDF[a-1]+(alphaPDF[a]+alphaPDF[a-1]) * \
+                      0.5*(alphas[a]-alphas[a-1])
+
+    aMin_index, aMax_index = getAlphaMinMaxIndices(E,beta,kb,T,A,alphas)
+    alphasToPlot, H = [], []
+    for a in range(aMin_index,aMax_index+1):
+        alphasToPlot.append(alphas[a])
+        H.append( (alphaCDF[a]-alphaCDF[aMin_index]) / \
+                  (alphaCDF[aMax_index]-alphaCDF[aMin_index]) )
+    
+
+    xsi_2 = random()
+    alpha = 0.0
+    for i in range(len(alphasToPlot)-1):
+        if alphas[i] >= alphasToPlot[0] and alphas[i] <= alphasToPlot[-1]:
+            if H[i] <= xsi_2 < H[i+1]:
+                frac  = (xsi_2-H[i])/(H[i+1]-H[i])
+                alpha = frac*(H[i+1]-H[i])+alphasToPlot[i]
+                break
+
+
+
+    plt.plot(alpha,beta,'ro')
+
+
+
+
+
+
+
+
+
+
+
 #plt.plot(relevantBetas,betaCDF)
-#plt.show()
+plt.xlabel('alpha')
+plt.ylabel('beta')
+plt.show()
 
 
 

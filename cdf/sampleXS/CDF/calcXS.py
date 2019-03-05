d6537714d028346a452b992c74b221ee31f0ae57
Amelia Trainer <ameliajo@mit.edu>
2019-03-05 11:10:04 -0500

    changing up convolution python script so its easier to compare various alphas

---
+++ cdf/sampleXS/CDF/calcXS.py
@@ -0,0 +1,271 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi
+from getSAB import *
+from rho import continRho,oscE,oscW
+import random
+
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+import seaborn as sns  # for nicer graphics
+
+
+
+
+def prepPlot(alphas):
+    cnorm = colors.Normalize(vmin=0,vmax=len(alphas)+2)
+    scalarMap = cmx.ScalarMappable(norm=cnorm,cmap=plt.get_cmap('hot')) #hot autumn tab10
+    #scalarMap = cmx.ScalarMappable(norm=cnorm,cmap=plt.get_cmap('tab10')) #hot autumn tab10
+    mymap = colors.LinearSegmentedColormap.from_list('funTestColors',\
+            [scalarMap.to_rgba(a) for a in range(len(alphas))])
+    colorBar = plt.contourf([[0,0],[0,0]], alphas, cmap=mymap)
+    plt.clf()
+    return scalarMap, colorBar
+
+
+
+def finishPlotting(colorBar,title):
+    ax = plt.gca()
+    plt.colorbar(colorBar).ax.set_ylabel('energies')
+    #plt.title(title)
+    #ax.set_facecolor('xkcd:light grey blue') # off white
+    #ax.set_facecolor('xkcd:very light blue') # off white
+    #plt.yscale('log')
+    plt.show()
+
+
+
+
+
+def getSABval(sab,a,b,nBeta):
+    return sab[a*nBeta+b]
+
+
+def getFullSAB(alphas,betas,fullBetas,sabs): 
+    fullSAB = [0.0]*(2*len(betas)-1)*len(alphas)
+    for a in range(len(alphas)):
+        for b in range(len(fullBetas)):
+            bForLookup = abs(b-len(betas)+1)
+            assert(abs(fullBetas[b]) == betas[bForLookup])
+            # This creates the symmetric SAB
+            fullSAB[a*len(fullBetas)+b] = getSABval(sabs,a,bForLookup,len(betas))
+            # We make it asymmetric to make sure it matches eq14
+            if (fullBetas[b] > 0): fullSAB[a*len(fullBetas)+b] *= np.exp(-fullBetas[b])
+    return fullSAB
+
+
+def getAlphaMinMax(E,beta,kb,T,A):
+    aMin = ( (E)**0.5 - (E+beta*kb*T)**0.5 )**2 / ( A*kb*T )
+    aMax = ( (E)**0.5 + (E+beta*kb*T)**0.5 )**2 / ( A*kb*T )
+    return aMin,aMax
+
+
+def getAlphaMinMaxIndices(E,beta,kb,T,A,alphas):
+    aMin,aMax = getAlphaMinMax(E,beta,kb,T,A)
+    if aMin < alphas[0] or aMax > alphas[-1]:
+        print("May want to have more alpha values")
+    aMin_index, aMax_index = 0, 0
+    for a in range(len(alphas)):
+        if alphas[a] > aMin and aMin_index == 0: aMin_index = a
+        if alphas[a] <= aMax:                    aMax_index = a
+    return aMin_index,aMax_index
+
+
+
+
+
+def getValidBetasRange(A,E,T,fullBetas):
+    kb = 8.6173303e-5
+    betaMin, betaMax = -E/(kb*T), 20.0
+    if betaMin < fullBetas[0] or betaMax > fullBetas[-1]:
+        print("May want to have more beta values")
+    bMin = bMax  = len(fullBetas)
+    for b in range(len(fullBetas)):
+        if bMin == len(fullBetas) and betaMin <= fullBetas[b]:
+            bMin = b
+        if fullBetas[b] >= betaMax:
+            return bMin,b
+    return bMin,bMax
+
+
+def intSABda(A,E,T,fullSAB,fullBetas,b):
+    kb = 8.6173303e-5
+    aMin,aMax = getAlphaMinMax(E,fullBetas[b],kb,T,A)
+    denominator = 0.0
+    for a in range(len(alphas)-1):
+        if aMin <= alphas[a] <= aMax:
+            sabL = getSABval(fullSAB,a,b,len(fullBetas))
+            sabR = getSABval(fullSAB,a+1,b,len(fullBetas))
+            denominator += (sabL+sabR)*0.5*(alphas[a+1]-alphas[a])
+    return denominator
+
+def calcBetaPDF(A,E,T,fullSAB,fullBetas,bMin,bMax):
+    delta0 = fullBetas[bMin+1]-fullBetas[bMin]
+    deltaN = fullBetas[bMax-1]-fullBetas[bMax-2]
+    denominator = intSABda(A,E,T,fullSAB,fullBetas,bMin)*delta0*0.5 +\
+                  intSABda(A,E,T,fullSAB,fullBetas,bMax-1)*deltaN*0.5
+    for b in range(bMin+1,bMax-1):
+        deltaL = fullBetas[b]-fullBetas[b-1]
+        deltaR = fullBetas[b+1]-fullBetas[b]
+        denominator += intSABda(A,E,T,fullSAB,fullBetas,b) * (deltaL+deltaR) * 0.5
+
+    eq14 = [ intSABda(A,E,T,fullSAB,fullBetas,b) / denominator \
+           for b in range(bMin,bMax-1)]
+    return eq14
+
+
+
+def calcBetaCDF(betas,eq14,bMin):
+    eq16 = [0.0]*len(eq14)
+    for b in range(1,len(eq14)):
+        eq16[b] = eq16[b-1] + (eq14[b-1]+eq14[b])*0.5 * \
+                  (fullBetas[bMin+b]-fullBetas[bMin+b-1]) 
+    return eq16
+
+
+
+
+
+def PDF_CDF_at_various_temperatures(A,E,T,fullSAB,fullBetas):
+    bMin,bMax = getValidBetasRange(A,E,T,fullBetas)
+    eq14 = calcBetaPDF(A,E,T,fullSAB,fullBetas,bMin,bMax)
+    eq16 = calcBetaCDF(fullBetas,eq14,bMin)
+    return fullBetas[bMin:bMax-1],eq16
+
+
+def sampleCDF(x,CDF):
+    xsi = random.random()
+    for i in range(len(x)-1):
+        if CDF[i] <= xsi < CDF[i+1]:
+            frac  = (xsi-CDF[i])/(CDF[i+1]-CDF[i])
+            val = frac*(x[i+1]-x[i])+x[i]
+            return i,val
+
+
+
+def getAlphaCDF(nAlpha,nFullBetas,index,fullSAB,alphas,aMin):
+    denominator = 0.0
+    for a in range(nAlpha-1):
+        sabL = getSABval(fullSAB,a,index,nFullBetas)
+        sabR = getSABval(fullSAB,a+1,index,nFullBetas)
+        denominator += (sabL+sabR)*0.5*(alphas[a+1]-alphas[a])
+
+    alphaPDF = [getSABval(fullSAB,a,index,nFullBetas)/denominator \
+                for a in range(nAlpha)]
+
+    alphaCDF = [0.0]*nAlpha
+    for a in range(1,nAlpha):
+        alphaCDF[a] = alphaCDF[a-1]+(alphaPDF[a]+alphaPDF[a-1]) * \
+                      0.5*(alphas[a]-alphas[a-1])
+    return alphaCDF
+
+
+
+
+A, T = 1.0, 296.0
+kb = 8.6173303e-5
+
+nAlpha, nBeta = 500, 500
+alphas = list(np.linspace(0.0001,30,nAlpha))
+betas  = list(np.linspace(0.00,30,nBeta))
+
+useNJOY  = True
+fullRedo = False  
+fullRedo = True
+width    = None 
+
+
+fullBetas = [-x for x in betas[1:]][::-1] + betas
+SAB = getSAB(alphas,betas,T,continRho,useNJOY,fullRedo,width,oscE,oscW) 
+fullSAB = getFullSAB(alphas,betas,fullBetas,SAB)
+
+N = 500
+N = 50000
+N = int(5e5)
+
+Energies = [0.0253]
+Energies = [0.005,0.01,0.95]
+Energies = [0.01]
+Energies = [3.12]
+Energies = [3.0]
+Energies = [0.005]
+Energies = [0.0253]
+Energies = [0.0005,0.0253,0.2907,0.95,3.12]
+#scalarMap, colorBar = prepPlot(Energies)
+
+
+for i,E in enumerate(Energies):
+    E_out_vec = []
+    betas_vec= []
+    print("E = ",E)
+
+    relevantBetas, betaCDF = PDF_CDF_at_various_temperatures(A,E,T,fullSAB,fullBetas)
+
+    xsi_Vals = []
+    for n in range(N):
+        index,beta = sampleCDF(relevantBetas,betaCDF)
+
+        """
+        aMin, aMax = getAlphaMinMaxIndices(E,beta,kb,T,A,alphas)
+        alphaCDF = getAlphaCDF(nAlpha,len(fullBetas),index,fullSAB,alphas,aMin)
+        # If the valid alpha values for this chosen beta are not going to occur
+        # we should just continue
+        if (alphaCDF[aMax] < 1e-12): continue
+        H = [None]*nAlpha
+        for a in range(nAlpha):
+            if   (alphas[a] <= alphas[aMin]): H[a] = 0
+            elif (alphas[a] >= alphas[aMax]): H[a] = 1
+            else: 
+                H[a] = ( (alphaCDF[a]-alphaCDF[aMin]) / \
+                         (alphaCDF[aMax]-alphaCDF[aMin]) )
+        index,alpha = sampleCDF(alphas,H)
+        """
+
+        E_out_vec.append((beta*kb*T+E))
+        #betas_vec.append((beta))
+
+
+    #sns.kdeplot(E_out_vec,shade=True);
+    #print(min(betas_vec),max(betas_vec))
+    #print(min(E_out_vec),max(E_out_vec))
+    #plt.hist(E_out_vec,bins=np.logspace(-3,1,500),normed=True,alpha=0.3)
+    #sns.kdeplot(E_out_vec,label=str(E)+' eV',shade=True,cut=10);
+    #sns.distplot(E_out_vec,bins=np.logspace(-3,1,2),hist=True,label=str(E)+' eV');
+    plt.hist(E_out_vec,normed=True,bins=np.logspace(-3,1,100),alpha=0.3)
+    #plt.hist(E_out_vec,bins=np.logspace(-3,1,500),normed=True,alpha=0.3)
+    #plt.hist(E_out_vec,bins=np.linspace(-40,5,100),alpha=0.3)
+    #plt.hist(E_out_vec,bins=np.linspace(0.0,3.5,100),alpha=0.3)
+
+#sns.kdeplot(E_out_vec);
+plt.gca().set_xscale("log")
+plt.yscale('log', nonposy='clip')
+
+#plt.xscale('log')
+#plt.yscale('log')
+
+axes = plt.gca()
+axes.set_xlim([1e-3,1e1])
+axes.set_ylim([1e-3,1e3])
+
+
+plt.show()
+
+
+
+
+#plt.plot(relevantBetas,betaCDF)
+#plt.xlabel('alpha')
+#plt.ylabel('beta')
+
+#finishPlotting(colorBar,"")
+
+
+
+
+
+
+
+
+
+
+
--- convolutionIntegral/convolution.py
+++ convolutionIntegral/convolution.py
@@ -3,9 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from plot import *
 
-
-# Lets assume an alpha value
-alpha = 0.50
 continRho = [0, .0005, .001, .002, .0035, .005, .0075, .01, .013, .0165, .02,  \
   .0245, .029, .034, .0395, .045, .0506, .0562, .0622, .0686, .075, .083, .091,\
   .099, .107, .115, .1197, .1214, .1218, .1195, .1125, .1065, .1005, .09542,   \
@@ -15,20 +12,23 @@ continRho = [0, .0005, .001, .002, .0035, .005, .0075, .01, .013, .0165, .02,  \
   .04244, .042, 0.0]
 
 rhoBeta = np.arange(0,0.00255*len(continRho),0.00255)
-P = [continRho[b]/(2*rhoBeta[b]*np.sinh(rhoBeta[b]*0.5)) for b in range(1,len(rhoBeta))]
-P = [continRho[1]/rhoBeta[1]**2] + P
+alpha = 3.0
 
-lambda_s = P[0] *2.0*np.cosh(rhoBeta[0 ]*0.5)*0.5*(rhoBeta[1] -rhoBeta[0] ) + \
-           P[-1]*2.0*np.cosh(rhoBeta[-1]*0.5)*0.5*(rhoBeta[-1]-rhoBeta[-2])
-for b in range(1,len(rhoBeta)-1):
-    lambda_s += P[b]*2.0*np.cosh(rhoBeta[b]*0.5)*(rhoBeta[b]-rhoBeta[b-1])
+def getT1(continRho,alpha,rhoBeta):
+    # Construct P(b) = rho(b) / 2*b*sinh(b/2)
+    P  = [continRho[1]/rhoBeta[1]**2] 
+    P += [continRho[b]/(2*rhoBeta[b]*np.sinh(rhoBeta[b]*0.5)) \
+            for b in range(1,len(rhoBeta))]
+    
+    lambda_s = P[0] *2.0*np.cosh(rhoBeta[0 ]*0.5)*0.5*(rhoBeta[1] -rhoBeta[0] ) + \
+               P[-1]*2.0*np.cosh(rhoBeta[-1]*0.5)*0.5*(rhoBeta[-1]-rhoBeta[-2])
 
-inputT1 = [P[b]*exp(-rhoBeta[b]*0.5)/lambda_s for b in range(len(rhoBeta))] 
+    for b in range(1,len(rhoBeta)-1):
+        lambda_s += P[b]*2.0*np.cosh(rhoBeta[b]*0.5)*(rhoBeta[b]-rhoBeta[b-1])
+    
+    inputT1 = [P[b]*exp(-rhoBeta[b]*0.5)/lambda_s for b in range(len(rhoBeta))] 
+    return inputT1,lambda_s
 
-#plt.plot(rhoBeta,continRho)
-#plt.plot(rhoBeta,P)
-#plt.plot(rhoBeta,T1)
-#plt.show()
 
 
 def interpolate(xList,yList,x):
@@ -62,31 +62,43 @@ def convolve(beta,T1,Tlast):
     return Tn
 
 
-numIter = 10
-scalarMap, colorBar = prepPlot(list(range(numIter)))
-beta = np.linspace(0,3,300) 
+inputT1,lambda_s = getT1(continRho,alpha,rhoBeta)
+
+beta = np.linspace(0,3,100) 
 N = len(beta)
+
+numIter = 50
+scalarMap, colorBar = prepPlot(list(range(numIter)))
 T1 = [interpolate(rhoBeta,inputT1,abs(beta[i])) for i in range(N)]
 Tn = T1[:]
-S = [0.0]*N
-S[0] = exp(-alpha*lambda_s)
-diffs = []
-for n in range(1,numIter):
-    alpha_n_term = (exp(-alpha*lambda_s)*(alpha*lambda_s)**n) * \
-                   (1.0/np.math.factorial(n))
-    totalDiff = 0.0
-    for b in range(N):
-        S[b] += alpha_n_term * Tn[b]
-        totalDiff += alpha_n_term * Tn[b] * 0.0001
-    diffs.append(totalDiff)
-    Tn = convolve(beta,T1,Tn)
-    plt.plot(beta,S,color=scalarMap.to_rgba(n))
-    if totalDiff < 1e-6:
-        break
-finishPlotting(colorBar)
+S = [exp(-alpha*lambda_s)] + [0.0]*(N-1)
+
+
+def doSum(numIter,alpha,lambda_s,T1,Tn,N):
+    diffs = []
+    for n in range(1,numIter):
+        alpha_n_term = (exp(-alpha*lambda_s)*(alpha*lambda_s)**n) * \
+                       (1.0/np.math.factorial(n))
+        totalDiff = 0.0
+        for b in range(N-1):
+            S[b] += alpha_n_term * Tn[b]
+            totalDiff += alpha_n_term * Tn[b] * (beta[b+1]-beta[b])
+        diffs.append(totalDiff)
+        Tn = convolve(beta,T1,Tn)
+        #plt.plot(beta,S,color=scalarMap.to_rgba(n))
+        #if totalDiff < 1e-6:
+        #    break
+    return diffs
+diffs = doSum(numIter,alpha,lambda_s,T1,Tn,N)
+#finishPlotting(colorBar)
 #plt.plot(diffs)
 #plt.show()
+#print(diffs)
 
+
+trueDiffs = [2.4121836009801084e-07, 2.2063872462386213e-06, 1.4594017441093847e-05, 7.4082832786500933e-05, 0.00030335272399319212, 0.0010392399199991198, 0.0030588047305379882, 0.0078899959373381675, 0.01811098956690866, 0.037447640715426382, 0.070438204669199797, 0.12151807124190461, 0.19360065951640482, 0.28651800519207349, 0.39588864651986738, 0.51296215871433704, 0.62570981687186966, 0.72098897017375563, 0.78719854279971324, 0.81664985299723114, 0.80698068786804744, 0.76128216839348695, 0.68703088370327159, 0.59425387602170132, 0.49349585949127134, 0.39409651135804924, 0.30308820384770885, 0.22478959687472144, 0.16098148749315844, 0.11145067692707357, 0.074675366176478616, 0.048474155171743728, 0.030514346663416101, 0.018644741008298982, 0.011067297813397185, 0.0063872401981762581, 0.0035867856509832724, 0.0019612567202264512, 0.0010449613497483085, 0.00054285960309393539, 0.0002751484361551681, 0.0001361433657559043, 6.579931947040709e-05, 3.1079648883037009e-05, 1.4354377732747896e-05, 6.485749329879233e-06, 2.8681921131320359e-06, 1.242009045359222e-06, 5.268631199836487e-07]
+for i in range(len(diffs)):
+    assert(abs((diffs[i]-trueDiffs[i])/trueDiffs[i])<1e-6)
 
 
 

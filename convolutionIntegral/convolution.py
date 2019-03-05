6b4f12cacea50e401cc8b57e46c70f5012582f9c
Amelia Trainer <ameliajo@mit.edu>
2019-03-05 11:39:37 -0500

    adding early comparing-sum-convergence-for-various-alphas testing

--- convolutionIntegral/convolution.py
+++ convolutionIntegral/convolution.py
@@ -12,7 +12,6 @@ continRho = [0, .0005, .001, .002, .0035, .005, .0075, .01, .013, .0165, .02,  \
   .04244, .042, 0.0]
 
 rhoBeta = np.arange(0,0.00255*len(continRho),0.00255)
-alpha = 3.0
 
 def getT1(continRho,alpha,rhoBeta):
     # Construct P(b) = rho(b) / 2*b*sinh(b/2)
@@ -45,10 +44,10 @@ def getVal(betaGrid,Tn,b):
     return Tn[b]
 
 
-def convolve(beta,T1,Tlast):
-    Tn = [0.0]*N
-    for b in range(N):
-        for bp in range(N-1):
+def convolve(beta,T1,Tlast,numBeta):
+    Tn = [0.0]*numBeta
+    for b in range(numBeta):
+        for bp in range(numBeta-1):
             AL = T1[bp]*exp(beta[bp])
             AR = T1[bp+1]*exp(beta[bp+1])
             BL = getVal(beta,Tlast,b+bp)
@@ -61,39 +60,95 @@ def convolve(beta,T1,Tlast):
                      (beta[bp+1]-beta[bp]) * 0.5
     return Tn
 
-
-inputT1,lambda_s = getT1(continRho,alpha,rhoBeta)
-
-beta = np.linspace(0,3,100) 
-N = len(beta)
-
-numIter = 50
-scalarMap, colorBar = prepPlot(list(range(numIter)))
-T1 = [interpolate(rhoBeta,inputT1,abs(beta[i])) for i in range(N)]
-Tn = T1[:]
-S = [exp(-alpha*lambda_s)] + [0.0]*(N-1)
-
-
-def doSum(numIter,alpha,lambda_s,T1,Tn,N):
-    diffs = []
+def getSum(alpha,beta,numIter=50):
+    inputT1,lambda_s = getT1(continRho,alpha,rhoBeta)
+    T1 = [interpolate(rhoBeta,inputT1,abs(beta[i])) for i in range(len(beta))]
+    Tn = T1[:]
+    S = [exp(-alpha*lambda_s)] + [0.0]*(len(beta)-1)
     for n in range(1,numIter):
         alpha_n_term = (exp(-alpha*lambda_s)*(alpha*lambda_s)**n) * \
                        (1.0/np.math.factorial(n))
-        totalDiff = 0.0
-        for b in range(N-1):
+        for b in range(len(beta)-1):
             S[b] += alpha_n_term * Tn[b]
-            totalDiff += alpha_n_term * Tn[b] * (beta[b+1]-beta[b])
-        diffs.append(totalDiff)
-        Tn = convolve(beta,T1,Tn)
-        #plt.plot(beta,S,color=scalarMap.to_rgba(n))
-        #if totalDiff < 1e-6:
-        #    break
+        Tn = convolve(beta,T1,Tn,len(beta))
+        plt.plot(beta,S,color=scalarMap.to_rgba(n))
     return diffs
-diffs = doSum(numIter,alpha,lambda_s,T1,Tn,N)
-#finishPlotting(colorBar)
+
+
+
+#numIter = 100
+#scalarMap, colorBar = prepPlot(list(range(numIter)))
+
+#alpha = 3.0
+#numBeta = 100
+#beta = np.linspace(0,30,numBeta) 
+
+def getConvergenceOfSum(alpha,beta,numIter):
+    inputT1,lambda_s = getT1(continRho,alpha,rhoBeta)
+    T1 = [interpolate(rhoBeta,inputT1,abs(beta[i])) for i in range(len(beta))]
+    Tn = T1[:]
+    diffs = [0.0]*(numIter-1)
+    for n in range(1,numIter):
+        alpha_n_term = (exp(-alpha*lambda_s)*(alpha*lambda_s)**n) * \
+                       (1.0/np.math.factorial(n))
+        diffs[n-1] = sum([alpha_n_term * Tn[b] * (beta[b+1]-beta[b]) \
+                          for b in range(len(beta)-1)])
+        Tn = convolve(beta,T1,Tn,len(beta))
+    return diffs
+#diffs = getConvergenceOfSum(alpha,beta,numIter)
 #plt.plot(diffs)
 #plt.show()
 #print(diffs)
+
+#peakLocations = []
+#for alpha in [0.001,0.05,0.1,1.0,3.0,5.0,10.0]:
+#    print(alpha)
+#    diffs = getConvergenceOfSum(alpha,beta,numIter)
+#    plt.plot([x/sum(diffs) for x in diffs])
+#    peakLocations.append(diffs.index(max(diffs)))
+#    
+
+#plt.show()
+
+
+#plt.plot(peakLocations)
+#plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+numIter = 50
+scalarMap, colorBar = prepPlot(list(range(numIter)))
+
+alpha = 3.0
+numBeta = 100
+beta = np.linspace(0,3,numBeta) 
+
+diffs = getConvergenceOfSum(alpha,beta,numIter)
+
+
+
 
 
 trueDiffs = [2.4121836009801084e-07, 2.2063872462386213e-06, 1.4594017441093847e-05, 7.4082832786500933e-05, 0.00030335272399319212, 0.0010392399199991198, 0.0030588047305379882, 0.0078899959373381675, 0.01811098956690866, 0.037447640715426382, 0.070438204669199797, 0.12151807124190461, 0.19360065951640482, 0.28651800519207349, 0.39588864651986738, 0.51296215871433704, 0.62570981687186966, 0.72098897017375563, 0.78719854279971324, 0.81664985299723114, 0.80698068786804744, 0.76128216839348695, 0.68703088370327159, 0.59425387602170132, 0.49349585949127134, 0.39409651135804924, 0.30308820384770885, 0.22478959687472144, 0.16098148749315844, 0.11145067692707357, 0.074675366176478616, 0.048474155171743728, 0.030514346663416101, 0.018644741008298982, 0.011067297813397185, 0.0063872401981762581, 0.0035867856509832724, 0.0019612567202264512, 0.0010449613497483085, 0.00054285960309393539, 0.0002751484361551681, 0.0001361433657559043, 6.579931947040709e-05, 3.1079648883037009e-05, 1.4354377732747896e-05, 6.485749329879233e-06, 2.8681921131320359e-06, 1.242009045359222e-06, 5.268631199836487e-07]

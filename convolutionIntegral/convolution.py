4c576779a0131f9b3a4cda7ca81deffe34b13f43
Amelia Trainer <ameliajo@mit.edu>
2019-03-05 12:20:58 -0500

    removed redundant lambda_s from contin equations

--- convolutionIntegral/convolution.py
+++ convolutionIntegral/convolution.py
@@ -25,12 +25,12 @@ def getT1(continRho,alpha,rhoBeta):
     for b in range(1,len(rhoBeta)-1):
         lambda_s += P[b]*2.0*np.cosh(rhoBeta[b]*0.5)*(rhoBeta[b]-rhoBeta[b-1])
     
-    inputT1 = [P[b]*exp(-rhoBeta[b]*0.5)/lambda_s for b in range(len(rhoBeta))] 
+    inputT1 = [P[b]*exp(-rhoBeta[b]*0.5) for b in range(len(rhoBeta))] 
     return inputT1,lambda_s
 
 
 
-def interpolate(xList,yList,x):
+def interpolate(xList,yList,x,lambda_s):
     for i in range(len(xList)-1):
         if (xList[i] <= x < xList[i+1]):
             m = (yList[i+1]-yList[i]) / (xList[i+1]-xList[i])
@@ -60,19 +60,19 @@ def convolve(beta,T1,Tlast,numBeta):
                      (beta[bp+1]-beta[bp]) * 0.5
     return Tn
 
-def getSum(alpha,beta,numIter=50):
-    inputT1,lambda_s = getT1(continRho,alpha,rhoBeta)
-    T1 = [interpolate(rhoBeta,inputT1,abs(beta[i])) for i in range(len(beta))]
-    Tn = T1[:]
-    S = [exp(-alpha*lambda_s)] + [0.0]*(len(beta)-1)
-    for n in range(1,numIter):
-        alpha_n_term = (exp(-alpha*lambda_s)*(alpha*lambda_s)**n) * \
-                       (1.0/np.math.factorial(n))
-        for b in range(len(beta)-1):
-            S[b] += alpha_n_term * Tn[b]
-        Tn = convolve(beta,T1,Tn,len(beta))
-        plt.plot(beta,S,color=scalarMap.to_rgba(n))
-    return diffs
+#def getSum(alpha,beta,numIter=50):
+#    inputT1,lambda_s = getT1(continRho,alpha,rhoBeta)
+#    T1 = [interpolate(rhoBeta,inputT1,abs(beta[i])) for i in range(len(beta))]
+#    Tn = T1[:]
+#    S = [exp(-alpha*lambda_s)] + [0.0]*(len(beta)-1)
+#    for n in range(1,numIter):
+#        alpha_n_term = (exp(-alpha*lambda_s)*(alpha)**n) * \
+#                       (1.0/np.math.factorial(n))
+#        for b in range(len(beta)-1):
+#            S[b] += alpha_n_term * Tn[b]
+#        Tn = convolve(beta,T1,Tn,len(beta))
+#        plt.plot(beta,S,color=scalarMap.to_rgba(n))
+#    return diffs
 
 
 
@@ -85,11 +85,11 @@ def getSum(alpha,beta,numIter=50):
 
 def getConvergenceOfSum(alpha,beta,numIter):
     inputT1,lambda_s = getT1(continRho,alpha,rhoBeta)
-    T1 = [interpolate(rhoBeta,inputT1,abs(beta[i])) for i in range(len(beta))]
+    T1 = [interpolate(rhoBeta,inputT1,abs(beta[i]),lambda_s) for i in range(len(beta))]
     Tn = T1[:]
     diffs = [0.0]*(numIter-1)
     for n in range(1,numIter):
-        alpha_n_term = (exp(-alpha*lambda_s)*(alpha*lambda_s)**n) * \
+        alpha_n_term = (exp(-alpha*lambda_s)*(alpha)**n) * \
                        (1.0/np.math.factorial(n))
         diffs[n-1] = sum([alpha_n_term * Tn[b] * (beta[b+1]-beta[b]) \
                           for b in range(len(beta)-1)])
@@ -153,10 +153,10 @@ diffs = getConvergenceOfSum(alpha,beta,numIter)
 
 trueDiffs = [2.4121836009801084e-07, 2.2063872462386213e-06, 1.4594017441093847e-05, 7.4082832786500933e-05, 0.00030335272399319212, 0.0010392399199991198, 0.0030588047305379882, 0.0078899959373381675, 0.01811098956690866, 0.037447640715426382, 0.070438204669199797, 0.12151807124190461, 0.19360065951640482, 0.28651800519207349, 0.39588864651986738, 0.51296215871433704, 0.62570981687186966, 0.72098897017375563, 0.78719854279971324, 0.81664985299723114, 0.80698068786804744, 0.76128216839348695, 0.68703088370327159, 0.59425387602170132, 0.49349585949127134, 0.39409651135804924, 0.30308820384770885, 0.22478959687472144, 0.16098148749315844, 0.11145067692707357, 0.074675366176478616, 0.048474155171743728, 0.030514346663416101, 0.018644741008298982, 0.011067297813397185, 0.0063872401981762581, 0.0035867856509832724, 0.0019612567202264512, 0.0010449613497483085, 0.00054285960309393539, 0.0002751484361551681, 0.0001361433657559043, 6.579931947040709e-05, 3.1079648883037009e-05, 1.4354377732747896e-05, 6.485749329879233e-06, 2.8681921131320359e-06, 1.242009045359222e-06, 5.268631199836487e-07]
 for i in range(len(diffs)):
-    assert(abs((diffs[i]-trueDiffs[i])/trueDiffs[i])<1e-6)
+    assert(abs((diffs[i]-trueDiffs[i])/trueDiffs[i])<1e-14)
 
 
-
+print(max(diffs))
 
 
 
